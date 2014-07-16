bddebc28a624a439466727d5f2f82b18511e41d1
Emerson Gutierrez <emergugue@gmail.com>
2014-07-16 10:22:10 -0500

    Se actualizó la informaión de Sala de Prensa

--- wp-content/themes/feria/category-3.php
+++ wp-content/themes/feria/category-3.php
@@ -129,68 +129,58 @@
    <span class="icono"></span>
    <?php if(qtrans_getLanguage() == 'es'): ?>
    <span>Kit de prensa</span>
- <?php else: ?>
- <span>Press kit</span>
-<?php endif; ?>         
-</div><!-- cierra .tit --> 
+   <?php else: ?>
+   <span>Press kit</span>
+    <?php endif; ?>         
+  </div><!-- cierra .tit --> 
 
-<div class="clearfix row-fluid">
-
-  <div class="span6 logoDescarga">
-    <span class="descarga">
+  <div class="clearfix row-fluid">
+    <div class="span6 logoDescarga">
+      <span class="descarga">
       <a href="<?php bloginfo('template_directory'); ?>/descargables/logos.zip"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar logo<?php else: ?>Download logo <?php endif; ?> </a></span>   
     </div>
-
-    <div class="span6 logosVarios">
-      <span class="descarga"><a href="<?php bloginfo('template_directory'); ?>/descargables/sellosCRV.zip"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar logos de eventos<?php else: ?>Download event logos<?php endif; ?></a></span> 
+    <div class="span6 galeriaFeriaD"> <!-- Galeria Feria 2012 -->
+      <iframe src="https://www.flickr.com/photos/alcaldiademed/9357630457/in/set-72157634775199157/player/" width="307" height="182" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+      <span class="descarga"><a href="https://www.flickr.com/photos/alcaldiademed/sets/72157634775199157/" target="_blank"><?php if(qtrans_getLanguage() == 'es'): ?>Ver galería: Feria de las flores 2012<?php else: ?>Gallery: Feria de las Flores 2012<?php endif; ?></a></span>
     </div>
-
+    <!-- <div class="span6 logosVarios">
+      <span class="descarga"><a href="<?php bloginfo('template_directory'); ?>/descargables/sellosCRV.zip"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar logos de eventos<?php else: ?>Download event logos<?php endif; ?></a></span> 
+    </div> -->
   </div><!-- cierra row -->
 
   <div class="clearfix row-fluid">
+    <div class="span6 galeriaFeriaD"><!-- Galeria Feria 2013 -->
+      <iframe src="https://www.flickr.com/photos/alcaldiademed/9477118921/in/set-72157634919666530/player/" width="307" height="182" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe><br />
+      <span class="descarga"><a href="https://www.flickr.com/photos/alcaldiademed/sets/72157634919666530/" target="_blank"><?php if(qtrans_getLanguage() == 'es'): ?>Ver galería: Desfile de silleteritos 2013<?php else: ?>Gallery: Desfile de silleteritos 2013<?php endif; ?></a></span>
+    </div>
+     <div class="span6 galeriaFeriaD"> <!-- Galería de Medellín -->
+      <iframe src="https://www.flickr.com/photos/alcaldiademed/11885064474/in/set-72157639647576673/player/" width="307" height="182" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+      <span class="descarga"><a href="https://www.flickr.com/photos/alcaldiademed/sets/72157639647576673/" target="_blank"><?php if(qtrans_getLanguage() == 'es'): ?>Ver galería: Imágenes de Medellín<?php else: ?>Gallery: Pictures of Medellín<?php endif; ?></a></span>
+    </div>
+  </div><!-- cierra row -->
+
+  <div class="clearfix row-fluid">
+    <div class="span6 logoDescarga"> <!-- Progrmación -->
+      <span class="descarga"><a href="<?php bloginfo('template_directory'); ?>/descargables/Programacion.pdf"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar programación<?php else: ?>Download Schedule<?php endif; ?></a></span>
+    </div>
+    <div class="span6 logoDescarga">
+      <span class="descarga"><a href="<?php bloginfo('template_directory'); ?>/descargables/Programacion_hd.pdf"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar programación HD<?php else: ?>Download Schedule HD<?php endif; ?></a></span>
+    </div>
+  </div><!-- cierra row -->
+
+  <div class="clearfix row-fluid">
+  </div><!-- cierra row -->
+  <div class="clearfix row-fluid" style="display:none;">
+     <div class="span6 videoFeria">
+      <iframe width="307" height="182" src="//www.youtube.com/embed/bQ2KKHzR2DA" frameborder="0" allowfullscreen></iframe>
+      <span class="descarga"><a href="#">Descargar video</a></span> 
+    </div>
 
 
-
-    <div class="span6 galeriaFeriaD"> <!-- Galeria Feria 2012 -->
-     <iframe src="https://www.flickr.com/photos/alcaldiademed/9357630457/in/set-72157634775199157/player/" width="307" height="182" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-     <span class="descarga"><a href="https://www.flickr.com/photos/alcaldiademed/sets/72157634775199157/" target="_blank"><?php if(qtrans_getLanguage() == 'es'): ?>Ver galería: Feria de las flores 2012<?php else: ?>Gallery: Feria de las Flores 2012<?php endif; ?></a></span>
-   </div>
-
-   <div class="span6 galeriaFeriaD"><!-- Galeria Feria 2013 -->
-    <iframe src="https://www.flickr.com/photos/alcaldiademed/9477118921/in/set-72157634919666530/player/" width="307" height="182" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe><br />
-    <span class="descarga"><a href="https://www.flickr.com/photos/alcaldiademed/sets/72157634919666530/" target="_blank"><?php if(qtrans_getLanguage() == 'es'): ?>Ver galería: Desfile de silleteritos 2013<?php else: ?>Gallery: Desfile de silleteritos 2013<?php endif; ?></a></span>
-  </div>
-
-</div><!-- cierra row -->
-
-<div class="clearfix row-fluid">
-  <div class="span6 galeriaFeriaD"> <!-- Galería de Medellín -->
-    <iframe src="https://www.flickr.com/photos/alcaldiademed/11885064474/in/set-72157639647576673/player/" width="307" height="182" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-    <span class="descarga"><a href="https://www.flickr.com/photos/alcaldiademed/sets/72157639647576673/" target="_blank"><?php if(qtrans_getLanguage() == 'es'): ?>Ver galería: Imágenes de Medellín<?php else: ?>Gallery: Pictures of Medellín<?php endif; ?></a></span>
-  </div>
-  <div class="span6 logoDescarga"> <!-- Progrmación -->
-    <span class="descarga"><a href="<?php bloginfo('template_directory'); ?>/descargables/Programacion.pdf"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar programación<?php else: ?>Download Schedule<?php endif; ?></a></span>
-  </div>
-</div><!-- cierra row -->
-
-<div class="clearfix row-fluid">
-  <div class="span6 logoDescarga">
-    <span class="descarga"><a href="<?php bloginfo('template_directory'); ?>/descargables/Programacion_hd.pdf"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar programación HD<?php else: ?>Download Schedule HD<?php endif; ?></a></span>
-  </div>
-</div><!-- cierra row -->
-
-
-<div class="clearfix row-fluid" style="display:none;">
-   <div class="span6 videoFeria">
-    <iframe width="307" height="182" src="//www.youtube.com/embed/bQ2KKHzR2DA" frameborder="0" allowfullscreen></iframe>
-    <span class="descarga"><a href="#">Descargar video</a></span> 
-  </div>
-
-
-  <div class="span6 logoDescarga">
-    <span class="descarga"><a href="#">Descargar cuña</a></span>
-  </div>
-</div><!-- cierra row -->
+    <div class="span6 logoDescarga">
+      <span class="descarga"><a href="#">Descargar cuña</a></span>
+    </div>
+  </div><!-- cierra row -->
 
 </div><!-- cierra .kitdeprensa -->
 </div>
