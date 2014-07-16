ba2ac01d1007dfd75aaa3cb9a917bf305985ce6e
Pablo Martínez <pablo.576@hotmail.com>
2014-07-15 19:58:20 -0500

    logos 2014

--- wp-content/themes/feria/category-3.php
+++ wp-content/themes/feria/category-3.php
@@ -138,7 +138,7 @@
 
   <div class="span6 logoDescarga">
     <span class="descarga">
-      <a href="<?php bloginfo('template_directory'); ?>/descargables/LogoFeria2013.zip"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar logo<?php else: ?>Download logo <?php endif; ?> </a></span>   
+      <a href="<?php bloginfo('template_directory'); ?>/descargables/logos.zip"><?php if(qtrans_getLanguage() == 'es'): ?>Descargar logo<?php else: ?>Download logo <?php endif; ?> </a></span>   
     </div>
 
     <div class="span6 logosVarios">
