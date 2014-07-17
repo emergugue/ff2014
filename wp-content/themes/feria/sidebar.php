72ba0a1b05eb4e56e0f5117026f46323274f9bea
Pablo Martínez <pablo.576@hotmail.com>
2014-07-16 20:39:16 -0500

    banners nuevos

--- wp-content/themes/feria/sidebar.php
+++ wp-content/themes/feria/sidebar.php
@@ -22,7 +22,7 @@
                              <iframe width="300" height="169" src="//www.youtube.com/embed/aam3ce4gpfM" frameborder="0" allowfullscreen></iframe>
                            -->
                            <a href="http://bit.ly/U7s2um" alt="Descargar">
-                              <img src="<?php echo home_url(); ?>/wp-content/themes/feria/images/descarga-flores.jpg" >
+                              <img target="_blank" src="<?php echo home_url(); ?>/wp-content/themes/feria/images/descarga-flores.jpg" >
                            </a>
                   </div>
 					
