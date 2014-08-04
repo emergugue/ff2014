2d6e8cb774c98123d63844a151010f1e23a4aa75
Pablo Martínez <pablo.576@hotmail.com>
2014-08-04 17:52:28 -0500

    1. pluggins de optimizacion de imagenes , js y css. 2. peticion de requerimiento de la alcaldia, quitar algunos banners y ademas poner 8 noticias intercaladas 1 telemedellin y 1 de la alcaldia.

--- wp-content/themes/feria/getFeed.php
+++ wp-content/themes/feria/getFeed.php
@@ -44,64 +44,117 @@ function fetch_feed2( $url ) {
  }
  
  $feed = fetch_feed('http://noticias.telemedellin.tv/tag/feriaflores/feed');
-  //$feed = fetch_feed2('http://www.medellin.gov.co/irj/servlet/prt/portal/prtroot/pcd!3aportal_content!2fMunicipioMedellin!2fRssServerComponentMig?nodo=Feria%20de%20las%20Flores'); // el feed que queremos mostrar
-  if(count($feed->errors) > 0){
-    $limit = 0;
+ $feed2 = fetch_feed('http://www.medellin.gov.co/irj/servlet/prt/portal/prtroot/pcd!3aportal_content!2fMunicipioMedellin!2fRssServerComponentMig?nodo=Feria%20de%20las%20Flores'); // el feed que queremos mostrar
+
+
+  if(count($feed->errors) > 0 && count($feed2->errors) > 0){
+    $limit  = 0;
+    $limit2 = 0;
   }
   else {
-    $limit = $feed->get_item_quantity(3); // especificamos el número de items a mostrar
-    $items = $feed->get_items(0, $limit); // se crea un array con los items
+    $limit  = $feed->get_item_quantity(4); // especificamos el número de items a mostrar
+    $items  = $feed->get_items(0, $limit); // se crea un array con los items
+
+    $limit2 = $feed2->get_item_quantity(4); // especificamos el número de items a mostrar
+    $items2 = $feed2->get_items(0, $limit2); // se crea un array con los items
   }
 }
-if ($limit == 0) echo '';
-else foreach ($items as $item) : ?>
-<div class="noticiaHome">
+if ($limit == 0 && $limit2 == 0 ) echo '';
+else foreach ($items as $key => $item) :?>
+
   <?php
-  $img =  stristr ( $item->get_content() , "<img");
 
+  $img    =  stristr ( $item->get_content() , "<img");
   $second = strpos($img, ">");
-
-  $img = substr($img, 0, $second);
-
-  $src = substr($img, strpos($img, "src=") + 5 ) ;
+  $img    = substr($img, 0, $second);
+  $src    = substr($img, strpos($img, "src=") + 5 ) ;
   $params = preg_split('[\"|\']', $src, 2);
+
+  $img2    = stristr ( $items2[$key]->get_content() , "<img");
+  $second2 = strpos($img2, ">");
+  $img2    = substr($img2, 0, $second2);
+  $src2    = substr($img2, strpos($img2, "src=") + 5 ) ;
+  $params2 = preg_split('[\"|\']', $src2, 2);
+
   if($params[0] == "" || $params[0] == "/" || $params[0] == " "){
     $src = get_template_directory_uri() . "/images/genericaFeed.jpg";
   }
   else{
     $src = $params[0];
   }
+
+ if($params2[0] == "" || $params2[0] == "/" || $params2[0] == " "){
+    $src2 = get_template_directory_uri() . "/images/genericaFeed.jpg";
+  }
+  else{
+    $src2 = $params2[0];
+  }
+
   ?>
-  <img src="<?php echo $src ?>" width="200" height="130" class="img-rounded" />
-  <h2><?php echo $item->get_title(); ?></h2>
-  <div class="linea hidden-phone"></div>
-  <p><?php echo substr(strip_tags ($item->get_content()), 0, 200) ?>...</p>
-  <div class="links">
-    <div class="redes">
-      <span class="twitter">
-        <a href="https://twitter.com/share?text=<?php echo $item->get_title(); ?>&url=<?php echo $item->get_permalink(); ?>" target="_blank">
-          Twitter
-        </a>
-      </span>
-      <span class="facebook">
-        <a target="_blank" href="http://www.facebook.com/sharer/sharer.php?s=100&amp;p[url]=<?php echo $item->get_permalink(); ?>&amp;p[title]=<?php echo $item->get_title(); ?>&amp;p[summary]=<?php echo substr(strip_tags ($item->get_content()), 0, 200) ?>">
-          Facebook
-        </a>
-      </span>
-    </div>
-    <?php if(qtrans_getLanguage() == 'es'): ?>
-    <div class="vermas">
-      <a target="_BLANK" href="<?php echo $item->get_permalink(); ?>" title="<?php echo $item->get_date('j F Y @ G:i'); ?>">Ver más</a>
-    </div>
+  <!-- Noticia telemedellin -->
+  <div class="noticiaHome">
+    <img src="<?php echo $src ?>" width="200" height="130" class="img-rounded" />
+    <h2><?php echo $item->get_title(); ?></h2>
+    <div class="linea hidden-phone"></div>
+    <p><?php echo substr(strip_tags ($item->get_content()), 0, 200) ?>...</p>
+    <div class="links">
+      <div class="redes">
+        <span class="twitter">
+          <a href="https://twitter.com/share?text=<?php echo $item->get_title(); ?>&url=<?php echo $item->get_permalink(); ?>" target="_blank">
+            Twitter
+          </a>
+        </span>
+        <span class="facebook">
+          <a target="_blank" href="http://www.facebook.com/sharer/sharer.php?s=100&amp;p[url]=<?php echo $item->get_permalink(); ?>&amp;p[title]=<?php echo $item->get_title(); ?>&amp;p[summary]=<?php echo substr(strip_tags ($item->get_content()), 0, 200) ?>">
+            Facebook
+          </a>
+        </span>
+      </div>
+      <?php if(qtrans_getLanguage() == 'es'): ?>
+      <div class="vermas">
+        <a target="_BLANK" href="<?php echo $item->get_permalink(); ?>" title="<?php echo $item->get_date('j F Y @ G:i'); ?>">Ver más</a>
+      </div>
     <?php else: ?>
-     <div class="readmore">
+    <div class="readmore">
       <a target="_BLANK" href="<?php echo $item->get_permalink(); ?>" title="<?php echo $item->get_date('j F Y @ G:i'); ?>">Read more</a>
     </div>
     <?php endif; ?>
+    </div>
   </div>
+
+  <!-- Noticia Alcaldia-->
+  <div class="noticiaHome">
+    <img src="<?php echo $src2 ?>" width="200" height="130" class="img-rounded" />
+    <h2><?php echo $items2[$key]->get_title(); ?></h2>
+    <div class="linea hidden-phone"></div>
+    <p><?php echo substr(strip_tags ($items2[$key]->get_content()), 0, 200) ?>...</p>
+    <div class="links">
+      <div class="redes">
+        <span class="twitter">
+          <a href="https://twitter.com/share?text=<?php echo $items2[$key]->get_title(); ?>&url=<?php echo $items2[$key]->get_permalink(); ?>" target="_blank">
+            Twitter
+          </a>
+        </span>
+        <span class="facebook">
+          <a target="_blank" href="http://www.facebook.com/sharer/sharer.php?s=100&amp;p[url]=<?php echo $items2[$key]->get_permalink(); ?>&amp;p[title]=<?php echo $items2[$key]->get_title(); ?>&amp;p[summary]=<?php echo substr(strip_tags ($items2[$key]->get_content()), 0, 200) ?>">
+            Facebook
+          </a>
+        </span>
+      </div>
+      <?php if(qtrans_getLanguage() == 'es'): ?>
+      <div class="vermas">
+        <a target="_BLANK" href="<?php echo $items2[$key]->get_permalink(); ?>" title="<?php echo $items2[$key]->get_date('j F Y @ G:i'); ?>">Ver más</a>
+      </div>
+    <?php else: ?>
+    <div class="readmore">
+      <a target="_BLANK" href="<?php echo $items2[$key]->get_permalink(); ?>" title="<?php echo $items2[$key]->get_date('j F Y @ G:i'); ?>">Read more</a>
+    </div>
+  <?php endif; ?>
+</div>
 </div>
 <?php endforeach; ?>
 <?php
+/*
 if(function_exists('fetch_feed')) {
   include_once(ABSPATH . WPINC . '/feed.php');               // hay que incluir esto
   $feed = fetch_feed('http://www.medellin.gov.co/irj/servlet/prt/portal/prtroot/pcd!3aportal_content!2fMunicipioMedellin!2fRssServerComponentMig?nodo=Feria%20de%20las%20Flores'); // el feed que queremos mostrar
@@ -153,4 +206,4 @@ else foreach ($items as $item) : ?>
     </div>
   </div>
 </div>
-<?php endforeach; ?>
+<?php endforeach; */?>
